44722622208f886e49a7281582d97532441dafe3
Alanrah <18210623224@163.com>
2018-01-15 22:33:02 +0800

    --

--- 1.cpp
+++ 1.cpp
@@ -29,11 +29,56 @@ public:
 	return result;
 }
 };
+/*
+这道题一看就知道用暴力搜索肯定没问题，而且猜到OJ肯定不会允许用暴力搜索这么简单的方法，
+于是去试了一下，果然是Time Limit Exceeded，这个算法的时间复杂度是O(n^2)。
+那么只能想个O(n)的算法来实现，整个实现步骤为：先遍历一遍数组，
+建立map数据，然后再遍历一遍，开始查找，找到则记录index。
+*/
+class Solution1 {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> m;
+        vector<int> res;
+        for (int i = 0; i < nums.size(); ++i) {
+            m[nums[i]] = i;
+        }
+        for (int i = 0; i < nums.size(); ++i) {
+            int t = target - nums[i];
+            if (m.count(t) && m[t] != i) {
+                res.push_back(i);
+                res.push_back(m[t]);
+                break;
+            }
+        }
+        return res;
+    }
+};
+//暴力解析
+class Solution2{
+public:
+    vector<int> twoSum(vector<int> &numbers, int target) {
+    int n = numbers.size();
+    vector<int> result;
+    for(int i = 0; i < n - 1; i++)
+    {
+        for(int j = i + 1; j < n; j++)
+        {
+            if(numbers[i] + numbers[j] == target)
+            {
+                result.push_back(i);
+                result.push_back(j);
+                return result;
+            }
+        }
+    }
+}
+};
 int main()
 {
-	int p[] = {3,3};
-    std::vector<int> myvector( p, p + 2 );
+	int p[] = {3,3,2};
+    std::vector<int> myvector( p, p + 3 );
     Solution S;
-    vector<int> result=S.twoSum(myvector,6);
+    vector<int> result=S.twoSum(myvector,5);
     cout<<result[0]<<"  "<<result[1];
 }
