01ee9e8070f59b28bb7c0a475773e36cbee6a0b3
Alanrah <18210623224@163.com>
2018-04-22 21:07:04 +0800

    422

---
+++ vue-learn-demo/vue-webpack-todo-app/src/app.vue
@@ -0,0 +1,17 @@
+<template>
+	<!--html代码-->
+	<div></div>
+</template>
+<script type="text/javascript">
+	//Js代码
+	export default{
+		data(){
+			return{
+				text:"abc"
+			}
+		}
+	}
+</script>
+<style type="text/css">
+	/*css代码*/
+</style>
